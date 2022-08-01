201639eccbb35ebb504e4aabe6c74698107d6d57
Naty Martorelli <nmartorelli@gmail.com>
2022-08-01 14:04:59 -0300

    ultimas modificaciones

--- js/libro4.js
+++ js/libro4.js
@@ -7,8 +7,11 @@ const API_GOOGLE_FREE = 'https://books.googleapis.com/books/v1/volumes?q=fantast
 
 const buscarLibro = () => {
   let search =  document.getElementById("search").value;
-	document.getElementById("results").innerHTML = ""
-    fetch(API_GOOGLE  + search)
+  if (search.length == 0 ) { 
+    Swal.fire('Por favor ingresá el título de un libro o un autor');
+}
+else {
+  fetch(API_GOOGLE  + search)
   .then((resp)=>{
     console.log(resp);
     return resp.json();
@@ -25,20 +28,25 @@ const buscarLibro = () => {
 
   })
 }
+}
 
 //pinto resultados en dom
 const result = (libros) => {	   
   for(i=0;i<10;i++){
-	let libro = libros.items[i].volumeInfo;	
+	let libro = libros.items[i].volumeInfo ;	
+  let venta = libros.items[i].saleInfo;
   let libroId = libros.items[i].id;	
+  //armo la tarjeta verificando los valores con nullish y acc.condicional
 	document.getElementById("results").innerHTML +=  `
         <div data-aos="flip-left" id= "libro${libroId}" class="card m-2 " style="width: 15rem;">
             <img src= ${libro.imageLinks?.thumbnail} class="card-img-top">
           <div class="card-body">
-            <h4> ${libro.title}</h4>
-              <h6 class="card-title text-success">Autor: ${libro.authors}</h5>
-              <small class="text-muted"> ID &nbsp; ${libroId} </small>
-                <p class="card-text">⭐${libro.averageRating}  </p>
+          <small class="text-muted"> ID &nbsp; ${libroId} </small>
+            <h5> ${libro.title}</h5>
+              <h6 class="card-title text-success">Autor: ${libro.authors}</h6>
+               <p class="card-text">${venta.saleability ?? "" } </p> 
+               <p class="card-text">${venta.listPrice?.currencyCode || "" } ${venta.listPrice?.amount || "" } </p> 
+                <p class="card-text">⭐${libro.averageRating ?? ""} </p>
                 <p class="card-text text-secondary">Género: ${libro.categories}</p>
                 <a href= ${libro.infoLink} class="btn btn-primary">Ver info</a>
                 <button id="fav${libroId}" onclick="afav" class="btn btn-primary"> 🤍 </button>
@@ -50,17 +58,17 @@ const result = (libros) => {
    // mandar a favoritos..(no me salió)
    //const afav = () => { };
 } 
-//activo búsqueda botón        
-button.addEventListener ("click", () => {
-  buscarLibro();
-  search.value = '';
-  localStorage.clear()
-});
+//activo búsqueda con botón       
+
+button.onclick = () => {
+    buscarLibro();
+    search.value = '';
+  }
 
 //Contenido Derecho Libros gratis
 let ebooksGratis = document.getElementById('favoritosbox');
 
-//fetch de libros gratis 1
+//fetch de libros gratis 
 const librosgratis = () => {
   fetch (API_GOOGLE_FREE)
   .then ((resp) => {
@@ -92,4 +100,55 @@ const librosgratis = () => {
     }
 }
   librosgratis();
+
+  const ATA = () => { alert ('esta')};
+  
+///Popup Sweetalert GITHUB REGALO
+function modalpop(){
+  Swal.fire({
+    title: '<h4>Tenés una cuenta de Github?</h4><p style="font-size:15px;">Ingresá tu usuario y llevate un regalo especial</p>',
+    input: 'text',
+    inputAttributes: {
+      autocapitalize: 'off'
+    },
+    showCancelButton: true,
+    confirmButtonText: 'Look up',
+    showLoaderOnConfirm: true,
+    preConfirm: (login) => {
+      return fetch(`//api.github.com/users/${login}`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(response.statusText)
+          }
+          return response.json()
+        })
+        .catch(error => {
+          Swal.showValidationMessage(
+            `Request failed: ${error}`
+          )
+        })
+    },
+    allowOutsideClick: () => !Swal.isLoading()
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: `Hola ${result.value.login}!`,
+        imageUrl: result.value.avatar_url,
+        html:
+          'Tengo este ebook de regalo para vos<br>, ' +
+          '<div class="libro-free"><img src="https://covers.zlibcdn2.com/covers299/books/b1/0d/8c/b10d8cb3ee92d824079354706a827f8c.jpg" width="50px;"></img><br>' + '<h5>Javascript Awesomness: Aprendé a escribir javascript de una manera increíble<br>' +
+          '<a href="https://es.ar1lib.org/dl/3429030/4fd607" download="proposed_file_name">Descargar Regalo</a></h5></div>',
+        showCloseButton: true,
+        showCancelButton: true,
+        focusConfirm: false,
+        cancelButtonText:
+          'No gracias',
+        cancelButtonAriaLabel: ''
+      })
+    }
+  })
+}
+
+
+setTimeout(modalpop, 2000);
 
